146dbc373a136a627d6480dda589b2c813530ba7
Dane Powell <git@danepowell.com>
2013-08-13 11:49:40 -0500

    Moved IM server into omni_im_demo.

--- README.md
+++ README.md
@@ -1,22 +1,10 @@
 omni_im
 =======
 
-Interactive Marker Server integrated with the Phantom Omni
+Interactive Marker (IM) Server integrated with the Phantom Omni
 
-This provides interactive marker server nodes, based on those in the interactive marker tutorials package, that allow control of interactive markers using the Phantom Omni.
+This provides a node (omni_im.py) that, when given the name of an IM feedback topic, will allow the user to control markers on that topic using a Phantom Omni.
 
-This is mostly a proof-of-concept package that you would need to adapt for use with an actual robot. Basically, you'd want to copy the callback functions into your own project's existing interactive marker server node, and modify them to send feedback to an arbitrary marker, rather than just "omni_im". (Maybe this can be pulled out into a separate package, so you don't even have to modify your existing server? Doubtful, but maybe...)
-
-To launch: roslaunch omni_im omni_im.launch
-This will launch rviz, add the Interactive Marker display, the whole #!.
-
-How it works:
-The launch file runs four nodes: the phantom omni and associated robot state publisher, omni_im.py, and rviz.
-
-Of course, omni_im is the guts of the operation. It subscribes to the omni's joint state topic. If the omni moves, and the user has currently selected omni control mode, the subscriber callback publishes feedback to the interactive marker feedback topic, based on the omni's position. This is what causes the marker to move (yay!).
-
-There's also a menu handler callback- this just keeps track of whether the user has selected the omni control mode or not.
-
-Finally, there's a interactive marker feedback callback. This just keeps track of the marker's pose, storing it as a tf. The main thread constantly publishes this tf.
+All you have to do is click on a marker to select it, and then press a button on the Omni to begin moving it (this is a 'clutch' system.)
 
 Behind the scenes (although visible if you look at the TFs), you'll see that this works by storing reference positions for the marker and stylus whenever you switch to omni control mode. While in omni control, the transform between the stylus reference and actual position is applied to the marker reference position to get the desired marker pose, which is published to the interactive marker feedback topic.
--- src/omni_im.py
+++ src/omni_im.py
@@ -6,22 +6,20 @@ from sensor_msgs.msg import JointState
 from phantom_omni.msg import PhantomButtonEvent
 import tf
 from interactive_markers.interactive_marker_server import *
-from interactive_markers.menu_handler import *
 import tf_conversions.posemath as pm
 
+marker_name = ''
+topic_name = rospy.get_param('~/omni_im/topic_name', '')
+
 # todo: get rid of all these globals- use classes instead
-omni_control = False
 listener = None
 br = None
-server = None
-menu_handler = MenuHandler()
-feedback_pub = rospy.Publisher('omni_im/feedback', InteractiveMarkerFeedback)
+feedback_pub = rospy.Publisher(topic_name + '/feedback', InteractiveMarkerFeedback)
 button_clicked = False
 feedback_client_id = '/rviz/InteractiveMarkers'
 
 def updateRefs():
     global marker_ref, stylus_ref
-
     try:
         stylus_ref = listener.lookupTransform('/world', '/stylus', rospy.Time(0))
         marker_ref = listener.lookupTransform('/world', '/marker', rospy.Time(0))
@@ -32,29 +30,11 @@ def checkFeedback(client_id):
     if client_id != feedback_client_id:
         rospy.logwarn("Different client_id! This could cause feedback to be ignored. i.e., break EVERYTHING.")
 
-# User clicked on menu entry, i.e. 'Omni Control'
-def processMenuFeedback(feedback):
-    global omni_control
-    if feedback.event_type == InteractiveMarkerFeedback.MENU_SELECT:
-        handle = feedback.menu_entry_id
-        state = menu_handler.getCheckState(handle)
-        if state == MenuHandler.CHECKED:
-            menu_handler.setCheckState(handle, MenuHandler.UNCHECKED)
-            omni_control = False
-        else:
-            menu_handler.setCheckState(handle, MenuHandler.CHECKED)
-            omni_control = True
-        menu_handler.reApply(server)
-    checkFeedback(feedback.client_id)
-    server.applyChanges()
-
 # Marker moved - just save its new pose
 def processMarkerFeedback(feedback):
-    global marker_tf
-    if feedback.event_type == InteractiveMarkerFeedback.POSE_UPDATE:
-        marker_tf = pm.toTf(pm.fromMsg(feedback.pose))
-    checkFeedback(feedback.client_id)
-    server.applyChanges()
+    global marker_tf, marker_name
+    marker_name = feedback.marker_name
+    marker_tf = pm.toTf(pm.fromMsg(feedback.pose))
 
 def omni_button_callback(button_event):
     global button_clicked
@@ -62,11 +42,9 @@ def omni_button_callback(button_event):
 
 # Gets called whenever omni position (joint state) changes
 # The idea here is that we publish the omni position to the omni_im feedback topic,
-# but only if 'omni_control' is currently selected.
 def omni_callback(joint_state):
     global feedback_pub
-
-    if omni_control and button_clicked:
+    if button_clicked:
         try:
             # Get pose corresponding to transform between base and proxy.
             p = pm.toMsg(pm.fromTf(listener.lookupTransform('/world', '/proxy', rospy.Time(0))))
@@ -74,7 +52,7 @@ def omni_callback(joint_state):
             # Construct feedback message.
             feedback = InteractiveMarkerFeedback()
             feedback.pose = p
-            feedback.marker_name = 'omni_im'
+            feedback.marker_name = marker_name
             feedback.event_type = feedback.POSE_UPDATE
             feedback.client_id = feedback_client_id
 
@@ -88,68 +66,6 @@ def omni_callback(joint_state):
 def sendTf(transform, target, source):
     global br
     br.sendTransform(transform[0], transform[1], rospy.Time.now(), target, source)
-
-def makeControl(w, x, y, z, name, mode):
-    control = InteractiveMarkerControl()
-    control.orientation.w = w
-    control.orientation.x = x
-    control.orientation.y = y
-    control.orientation.z = z
-    control.name = name
-    control.interaction_mode = mode
-    return control
-
-def makeMarker():
-    # create an interactive marker for our server
-    int_marker = InteractiveMarker()
-    int_marker.header.frame_id = '/world'
-    int_marker.name = 'omni_im'
-    int_marker.description = "Phantom Omni Control"
-    int_marker.scale = 0.1
-
-    # create a grey box marker
-    box_marker = Marker()
-    box_marker.type = Marker.CUBE
-    box_marker.scale.x = 0.045
-    box_marker.scale.y = 0.045
-    box_marker.scale.z = 0.045
-    box_marker.color.r = 0.0
-    box_marker.color.g = 0.5
-    box_marker.color.b = 0.5
-    box_marker.color.a = 1.0
-
-    control = InteractiveMarkerControl()
-    control.always_visible = True
-    control.interaction_mode = InteractiveMarkerControl.MOVE_ROTATE_3D
-    control.markers.append(box_marker)
-    int_marker.controls.append(control)
-
-    control = makeControl(1, 1, 0, 0, 'rotate_x', InteractiveMarkerControl.ROTATE_AXIS)
-    int_marker.controls.append(control)
-    
-    control = makeControl(1, 1, 0, 0, 'move_x', InteractiveMarkerControl.MOVE_AXIS)
-    int_marker.controls.append(control)
-    
-    control = makeControl(1, 0, 1, 0, 'rotate_z', InteractiveMarkerControl.ROTATE_AXIS)
-    int_marker.controls.append(control)
-
-    control = makeControl(1, 0, 1, 0, 'move_z', InteractiveMarkerControl.MOVE_AXIS)
-    int_marker.controls.append(control)
-
-    control = makeControl(1, 0, 0, 1, 'rotate_y', InteractiveMarkerControl.ROTATE_AXIS)
-    int_marker.controls.append(control)
-
-    control = makeControl(1, 0, 0, 1, 'move_y', InteractiveMarkerControl.MOVE_AXIS)
-    int_marker.controls.append(control)
-
-    menu_control = InteractiveMarkerControl()
-    menu_control.name = 'menu_only_control'
-    menu_control.interaction_mode = InteractiveMarkerControl.BUTTON
-    menu_control.always_visible = True;
-    int_marker.controls.append(menu_control)
-
-    server.insert(int_marker, processMarkerFeedback)
-    menu_handler.apply(server, int_marker.name)
 
 if __name__=='__main__':
     global marker_tf, zero_tf, marker_ref, stylus_ref
@@ -166,17 +82,8 @@ if __name__=='__main__':
 
     rospy.Subscriber('omni1_joint_states', JointState, omni_callback)
     rospy.Subscriber('omni1_button', PhantomButtonEvent, omni_button_callback)
-
-    # create an interactive marker server on the topic namespace omni_im
-    server = InteractiveMarkerServer('omni_im')
-
-    entry = menu_handler.insert("Omni control", callback=processMenuFeedback)
-    menu_handler.setCheckState(entry, MenuHandler.UNCHECKED)
-    makeMarker()
-
-    # 'commit' changes and send to all clients
-    server.applyChanges()
-    
+    rospy.Subscriber(topic_name + '/feedback', InteractiveMarkerFeedback, processMarkerFeedback)
+    rospy.loginfo(topic_name)
     rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
         sendTf(marker_tf, '/marker', '/world')
