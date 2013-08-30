0d3c86eb8ab89fd62b17b8d0fb0116b86cf85f75
Dane Powell <git@danepowell.com>
2013-08-30 16:23:27 -0500

    Switched to using interaction_cursor_rviz as the frontend.

--- README.md
+++ README.md
@@ -3,15 +3,11 @@ omni_im
 
 Interactive Marker (IM) Server integrated with the Phantom Omni
 
-This provides a node (omni_im.py) that, when given the name of an IM feedback topic, will allow the user to control markers on that topic using a Phantom Omni.
+This package provides a backend for the interaction_cursor_rviz package, allowing you to control the 3D cursor using the Phantom Omni. Download the omni_im_demo package as well, and simply:
 
-All you have to do is click on a marker to select it, and then press a button on the Omni to begin moving it (this is a 'clutch' system.)
+roslaunch omni_im_demo omni_im_demo
+
+Then add the InteractionCursor display to rviz.
 
 Behind the scenes (although visible if you look at the TFs), you'll see that this works by storing reference positions for the marker and stylus whenever you switch to omni control mode. While in omni control, the transform between the stylus reference and actual position is applied to the marker reference position to get the desired marker pose, which is published to the interactive marker feedback topic.
 
-To try it out, download omni_im_demo, and simply:
-roslaunch omni_im_demo omni_im_demo.launch
-
-There are a couple of problems with this that I'm trying to address in feature branches:
-1) Latency. There's a lot of lag between input from Phantom and update to the interactive marker. I think this is because we don't have control over when the server applies updates. This is more of a problem in big simulations (i.e. PR2). However, I've found that the lag is also there using InteractionCursor, so maybe it's not actually a problem with my approach...
-2) Constraints. This completely 'forges' updates to the marker, bypassing the actual marker controls, meaning that it doesn't obey constraints imposed on the marker (such as to only translate or rotate in one dimension.)
--- src/omni_im.py
+++ src/omni_im.py
@@ -7,34 +7,33 @@ from phantom_omni.msg import PhantomButtonEvent
 import tf
 from interactive_markers.interactive_marker_server import *
 import tf_conversions.posemath as pm
+from interaction_cursor_msgs.msg import InteractionCursorUpdate
 
 marker_name = ''
 topic_name = rospy.get_param('~/omni_im/topic_name', '')
+fixed_frame = rospy.get_param('~/omni_im/fixed_frame', '/world')
+last_button_state = 0
 
 # todo: get rid of all these globals- use classes instead
 listener = None
 br = None
-feedback_pub = rospy.Publisher(topic_name + '/feedback', InteractiveMarkerFeedback)
+update_pub = rospy.Publisher('/interaction_cursor/update', InteractionCursorUpdate)
 button_clicked = False
 feedback_client_id = '/rviz/InteractiveMarkers'
 
 def updateRefs():
     global marker_ref, stylus_ref
     try:
-        stylus_ref = listener.lookupTransform('/world', '/stylus', rospy.Time(0))
-        marker_ref = listener.lookupTransform('/world', '/marker', rospy.Time(0))
+        stylus_ref = listener.lookupTransform(fixed_frame, '/stylus', rospy.Time(0))
+        marker_ref = listener.lookupTransform(fixed_frame, '/marker', rospy.Time(0))
     except:
         pass
-
-def checkFeedback(client_id):
-    if client_id != feedback_client_id:
-        rospy.logwarn("Different client_id! This could cause feedback to be ignored. i.e., break EVERYTHING.")
 
 # Marker moved - just save its new pose
 def processMarkerFeedback(feedback):
     global marker_tf, marker_name
     marker_name = feedback.marker_name
-    marker_tf = pm.toTf(pm.fromMsg(feedback.pose))
+    marker_tf = ((feedback.pose.position.x, feedback.pose.position.y, feedback.pose.position.z), tf.transformations.quaternion_from_euler(0, 0, 0))
 
 def omni_button_callback(button_event):
     global button_clicked
@@ -43,25 +42,57 @@ def omni_button_callback(button_event):
 # Gets called whenever omni position (joint state) changes
 # The idea here is that we publish the omni position to the omni_im feedback topic,
 def omni_callback(joint_state):
-    global feedback_pub
-    if button_clicked:
-        try:
+    global update_pub, last_button_state
+    sendTf(marker_tf, '/marker', fixed_frame)
+    sendTf(zero_tf, '/base', fixed_frame)
+    sendTf(marker_ref, '/marker_ref', fixed_frame)
+    sendTf(stylus_ref, '/stylus_ref', fixed_frame)
+        
+    try:
+        rel_tf = listener.lookupTransform('/stylus_ref', '/stylus', rospy.Time(0))
+        sendTf(rel_tf, '/proxy', '/marker_ref')
+    except:
+        rospy.logerr("Couldn't look up second transform")
+
+    try:
+        update = InteractionCursorUpdate()
+        update.pose.header = std_msgs.msg.Header()
+        update.pose.header.stamp = rospy.Time.now()
+        update.pose.header.frame_id = 'marker_ref'
+        if button_clicked and last_button_state == update.GRAB:
+            update.button_state = update.KEEP_ALIVE
+        elif button_clicked and last_button_state == update.KEEP_ALIVE:
+            update.button_state = update.KEEP_ALIVE
+        elif button_clicked:
+            update.button_state = update.GRAB
+        elif last_button_state == update.KEEP_ALIVE:
+            update.button_state = update.RELEASE
+        else:
+            update.button_state = update.NONE
+            updateRefs()
+        update.key_event = 0
+
+        if button_clicked:
             # Get pose corresponding to transform between base and proxy.
-            p = pm.toMsg(pm.fromTf(listener.lookupTransform('/world', '/proxy', rospy.Time(0))))
+            p = pm.toMsg(pm.fromTf(listener.lookupTransform('/stylus_ref', '/stylus', rospy.Time(0))))
+        else:
+            p = pm.toMsg(pm.fromTf(zero_tf))
+        
+        # Simply scale this up a bit to increase the workspace.
+        workspace = 4
+        p.position.x = p.position.x * workspace
+        p.position.y = p.position.y * workspace
+        p.position.z = p.position.z * workspace
 
-            # Construct feedback message.
-            feedback = InteractiveMarkerFeedback()
-            feedback.pose = p
-            feedback.marker_name = marker_name
-            feedback.event_type = feedback.POSE_UPDATE
-            feedback.client_id = feedback_client_id
+        update.pose.pose = p
 
-            # Publish feedback.
-            feedback_pub.publish(feedback)
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            rospy.logerr("Couldn't look up transform. These things happen...")
-    else:
-        updateRefs()
+        last_button_state = update.button_state
+
+        # Publish feedback.
+        update_pub.publish(update)
+    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+        rospy.logerr("Couldn't look up transform. These things happen...")
+
 
 def sendTf(transform, target, source):
     global br
@@ -83,16 +114,5 @@ if __name__=='__main__':
     rospy.Subscriber('omni1_joint_states', JointState, omni_callback)
     rospy.Subscriber('omni1_button', PhantomButtonEvent, omni_button_callback)
     rospy.Subscriber(topic_name + '/feedback', InteractiveMarkerFeedback, processMarkerFeedback)
-    rate = rospy.Rate(10.0)
-    while not rospy.is_shutdown():
-        sendTf(marker_tf, '/marker', '/world')
-        sendTf(zero_tf, '/base', '/world')
-        sendTf(marker_ref, '/marker_ref', '/world')
-        sendTf(stylus_ref, '/stylus_ref', '/world')
-        
-        try:
-            rel_tf = listener.lookupTransform('/stylus_ref', '/stylus', rospy.Time(0))
-            sendTf(rel_tf, '/proxy', '/marker_ref')
-        except:
-            continue
-        rate.sleep()
+    
+    rospy.spin()
